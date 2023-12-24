8ae22319de311eba36f047109bfef3ac60fc42cc
Artuwo <artyr007@gmail.com>
2023-12-24 17:25:39 +0500

    [ADD] web base for astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://arthwr.github.io',
-  integrations: [tailwind()]
+  site: "https://arthwr.github.io",
+  base: "/geo-service",
+  integrations: [tailwind()],
 });
